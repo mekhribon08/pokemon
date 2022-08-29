5e91ecaabeb85b9d0ed5822abeb42ff5269a40b3
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2022-08-29 14:41:44 +0500

    pokemon

--- js/main.js
+++ js/main.js
@@ -4,34 +4,37 @@ var elInputName = document.querySelector("[data-name]");
 var elInputType = document.querySelector("[data-type]");
 var elInputWeight = document.querySelector("[data-weight]");
 var elInputHeight = document.querySelector("[data-height]");
+var elDivWrap = document.querySelector("[data-div-wrap]");
+var elDivCard = document.querySelector("[data-div-card]");
 
 for (var i = 0; i < pokemons.length; i++) {
-  var pokemons = pokemons[i];
+  var pokemon = pokemons[i];
 
-  var elDivWrap = document.createElement("div");
-  var elDivCard = document.createElement("div");
   var elImg = document.createElement("img");
   var elDivBody = document.createElement("div");
   var elh5 = document.createElement("h5");
   var elP = document.createElement("p");
-  var elP1 = document.createElement("p");
+  var elh6 = document.createElement("h6");
   var elSpan = document.createElement("span");
   var elSpan1 = document.createElement("span");
   var elBtn = document.createElement("button");
 
-  elImg.src = `${pokemons.img}`;
-  elImg.alt = pokemons.title;
-  elh5.textContent = `${pokemons.name}`;
-  elP.textContent = joinArray([pokemons.type]);
-  elSpan.textContent = `${pokemons.weight}`;
-  elSpan1.textContent = `${pokemons.height}`;
+  elImg.src = `${pokemon.img}`;
+  elImg.alt = pokemon.title;
+  elh5.textContent = `${pokemon.name}`;
+  elP.textContent = joinArray([pokemon.type]);
+  elSpan.textContent = `${pokemon.weight}`;
+  elSpan1.textContent = `${pokemon.height}`;
   elBtn.textContent = "Delate";
 
-  elDivBody.append(elImg);
-  elP1.append(elSpan, elSpan1);
-  elDivBody.append(elh5, elP, elP1, elBtn);
-  elDivCard.append(elImg, elDivCard);
-  elDivWrap.append(elDivCard);
+  elImg.classList.add("card-img-top");
+  elDivBody.classList.add("card-body");
+  elBtn.classList.add("btn-danger");
+
+  elh6.append(elSpan, elSpan1);
+  elDivBody.append(elh5, elP, elh6, elBtn);
+  elDivCard.append(elImg, elDivBody);
+  // elDivWrap.append(elDivCard);
 }
 
 function joinArray(arr, separator = "") {
