d11155ce37c86bf64e92eba50f5dc204e06cc9bd
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2023-01-08 22:37:00 +0500

    pokemon

--- js/main.js
+++ js/main.js
@@ -11,6 +11,52 @@ var elTypeSearchInput = document.querySelector("[data-type-search]");
 var elSelectType = document.querySelector("[data-select]");
 var elAl = document.querySelector("[data-alpha]");
 var elOption = document.querySelector("[data-option]");
+var elFavList = document.querySelector("[data-favourites-list]");
+
+const favourites = getFavourites();
+
+renderFav(favourites);
+
+elDivWrap.addEventListener("click", (evt) => {
+  onFavouriteClick(evt);
+});
+
+function onFavouriteClick(evt) {
+  const el = evt.target.closest("[data-card-add]");
+
+  if (!el) return;
+
+  const id = +el.dataset.id;
+  if (favourites.includes(id)) {
+    favourites.splice(favourites.indexOf(id), 1);
+  } else {
+    favourites.push(id);
+  }
+  setFavourites(favourites);
+
+  renderPokemons(pokemons);
+}
+
+function setFavourites(favourites) {
+  localStorage.setItem("favourites", JSON.stringify(favourites));
+  renderFav(favourites);
+}
+
+function renderFav(favourites) {
+  let html = "";
+
+  favourites.forEach((item) => {
+    let pokemon = pokemons.find((pokemon) => pokemon.id === item);
+    html += `<span class="badge bg-success m-2">${pokemon.name}</span>`;
+  });
+
+  elFavList.innerHTML = html;
+}
+
+function getFavourites() {
+  const stringFav = localStorage.getItem("favourites") || "[]";
+  return JSON.parse(stringFav);
+}
 
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -57,6 +103,13 @@ function createDiv(pokemon) {
 
   elCardImg.src = pokemon.img;
   elCardImg.alt = pokemon.name;
+  elCard.querySelector("[data-card-delate]").dataset.id = pokemon.id;
+  elCard.querySelector("[data-card-add]").dataset.id = pokemon.id;
+  elCard.querySelector("[data-card-add]").textContent = favourites.includes(
+    pokemon.id
+  )
+    ? "Added"
+    : "Add";
   elCard.querySelector("[data-card-title]").textContent = pokemon.name;
   elCard.querySelector("[data-card-type]").textContent = joinArray(
     pokemon.type,
@@ -133,4 +186,36 @@ function joinArray(arr, separator = "") {
     }
   }
   return str;
+}
+
+document.addEventListener("click", (evt) => {
+  orModalBtnClick(evt);
+  onModalOutsideClick(evt);
+  onModalCloseClick(evt);
+});
+
+function orModalBtnClick(evt) {
+  const el = evt.target.closest("[data-modal-open]");
+
+  if (!el) return;
+
+  const modalSel = el.dataset.modalOpen;
+
+  document.querySelector(modalSel).classList.add("show");
+}
+
+function onModalOutsideClick(evt) {
+  const el = evt.target;
+
+  if (!el.matches("[data-modal]")) return;
+
+  el.classList.remove("show");
+}
+
+function onModalCloseClick(evt) {
+  const el = evt.target.closest("[data-modal-close]");
+
+  if (!el) return;
+
+  el.parentElement.parentElement.classList.remove("show");
 }
